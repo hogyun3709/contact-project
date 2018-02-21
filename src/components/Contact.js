9b2815971e0043062d5d520c85a184275d42c5e6
hogyun3709 <hogyu3709@gmail.com>
2018-02-22 01:41:29 +0900

    Contact create, remove, update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
   constructor(props){
@@ -24,6 +25,9 @@ export default class Contact extends React.Component {
       };
       this.handleChange = this.handleChange.bind(this);
       this.handleClick = this.handleClick.bind(this);
+      this.handleCreate = this.handleCreate.bind(this);
+      this.handleRemove = this.handleRemove.bind(this);
+      this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleChange(e){
@@ -38,6 +42,33 @@ export default class Contact extends React.Component {
 
      console.log(key, 'is selected');
    }
+    handleCreate(contact){
+      this.setState({
+        contactData: update(this.state.contactData,
+           { $push: [contact]})
+      });
+    }
+    handleRemove(){
+      this.setState({
+        contactData: update(this.state.contactData,
+           { $splice: [[this.state.selectedKey, 1]]}
+         ),
+         selectedKey: -1
+       });
+    }
+
+    handleEdit(name, phone){
+      this.setState({
+        contactData: update(this.state.contactData,
+          {
+            [this.state.selectedKey]: {
+              name: {$set: name},
+              phone: {$set: phone}
+            }
+          }
+        )
+      })
+    }
 
   render(){
     const mapToComponents = (data) => {
